431698ba2eb3c5972dac53ba7a9be89739e62cad
malick tkr <malick.tounkara.1@gmiail.com>
2021-08-21 23:12:52 +0100

    manage user step 0

--- src/Form/PaymentType.php
+++ src/Form/PaymentType.php
@@ -4,13 +4,13 @@ namespace App\Form;
 
 use App\Entity\Payment;
 use App\Entity\PaymentMethod;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PaymentType extends AbstractType
 {
---
+++ src/Form/User1Type.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class User1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            // ->add('roles')
+            // ->add('password')
+            ->add('isVerified')
+            ->add('personne',PersonneType::class,[
+                'label'=>false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
